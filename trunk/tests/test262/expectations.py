f48852db84cb6a37c45528a1a26597e5d870c330
cira@google.com <cira@google.com@24c52dca-d3ef-abce-e343-6dbe4d066545>
2012-09-06 21:55:19 +0000

    Add check after constructing percent formatter. Add tests to exceptions list.

--- trunk/tests/test262/expectations.py
+++ trunk/tests/test262/expectations.py
@@ -16,14 +16,34 @@
 
 # Bugs
 expect_fail = {
-  'data/test/suite/intl402/ch11/11.2/11.2.1.js': 'FAIL',
-  'data/test/suite/intl402/ch11/11.3/11.3.js': 'FAIL',
-  'data/test/suite/intl402/ch12/12.2/12.2.1.js': 'FAIL',
-  'data/test/suite/intl402/ch12/12.3/12.3.2_3.js': 'FAIL',
-  'data/test/suite/intl402/ch12/12.3/12.3.2_5_b.js': 'FAIL',
-  'data/test/suite/intl402/ch12/12.3/12.3.js': 'FAIL',
-  'data/test/suite/intl402/ch13/13.2/13.2.1.js': 'FAIL',
-  'data/test/suite/intl402/ch13/13.3/13.3.js': 'FAIL'
+  # ISO and CLDR data mismatch.
+  'data/test/suite/intl402/ch11/11.1/11.1.1_20_c.js': 'FAIL',
+  # ICU bug http://bugs.icu-project.org/trac/ticket/9547
+  'data/test/suite/intl402/ch11/11.3/11.3.2_TRP.js': 'FAIL',
+  # ICU bug http://bugs.icu-project.org/trac/ticket/9265
+  'data/test/suite/intl402/ch09/9.2/9.2.5_11_g_ii_2.js': 'FAIL',
+  # Not implemented yet (overrides for toLocaleDate...)
+  'data/test/suite/intl402/ch13/13.1/13.1.1_6.js': 'FAIL',
+  'data/test/suite/intl402/ch13/13.1/13.1.1_7.js': 'FAIL',
+  'data/test/suite/intl402/ch13/13.2/13.2.1_1.js': 'FAIL',
+  'data/test/suite/intl402/ch13/13.2/13.2.1_4.js': 'FAIL',
+  'data/test/suite/intl402/ch13/13.2/13.2.1_5.js': 'FAIL',
+  'data/test/suite/intl402/ch13/13.3/13.3.0_6.js': 'FAIL',
+  'data/test/suite/intl402/ch13/13.3/13.3.0_7.js': 'FAIL',
+  # V8 issue - can't detect if a function is called as a constructor.
+  'data/test/suite/intl402/ch10/10.2/10.2.2_L15.js': 'FAIL',
+  'data/test/suite/intl402/ch10/10.3/10.3.2_1_a_L15.js': 'FAIL',
+  'data/test/suite/intl402/ch11/11.2/11.2.2_L15.js': 'FAIL',
+  'data/test/suite/intl402/ch11/11.3/11.3.2_1_a_L15.js': 'FAIL',
+  'data/test/suite/intl402/ch12/12.2/12.2.2_L15.js': 'FAIL',
+  'data/test/suite/intl402/ch12/12.3/12.3.2_1_a_L15.js': 'FAIL',
+  # Can't delete prototype property from non-constructor function.
+  'data/test/suite/intl402/ch10/10.3/10.3.2_L15.js': 'FAIL',
+  'data/test/suite/intl402/ch10/10.3/10.3.3_L15.js': 'FAIL',
+  'data/test/suite/intl402/ch11/11.3/11.3.2_L15.js': 'FAIL',
+  'data/test/suite/intl402/ch11/11.3/11.3.3_L15.js': 'FAIL',
+  'data/test/suite/intl402/ch12/12.3/12.3.2_L15.js': 'FAIL',
+  'data/test/suite/intl402/ch12/12.3/12.3.3_L15.js': 'FAIL',
 }
 
 # Deliberate incompatibilities
