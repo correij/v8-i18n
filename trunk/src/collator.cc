c08c4ebe2c831f9b350ceb758cd0f27ae9e0162f
cira@google.com <cira@google.com@24c52dca-d3ef-abce-e343-6dbe4d066545>
2012-11-20 19:31:01 +0000

    Fix for b/161999, empty ObjectTemplate instance handle.

--- trunk/src/collator.cc
+++ trunk/src/collator.cc
@@ -120,6 +120,12 @@ v8::Handle<v8::Value> Collator::JSCreateCollator(
 
   // Create an empty object wrapper.
   v8::Local<v8::Object> local_object = intl_collator_template->NewInstance();
+  // But the handle shouldn't be empty.
+  // That can happen if there was a stack overflow when creating the object.
+  if (local_object.IsEmpty()) {
+    return local_object;
+  }
+
   v8::Persistent<v8::Object> wrapper =
       v8::Persistent<v8::Object>::New(local_object);
 
